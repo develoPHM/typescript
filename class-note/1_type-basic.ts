565f0e630c51384d3227371efd5c9712941037bf
phmok1@naver.com <phmok1@naver.com>
2024-10-21 17:55:39 +0900

    ts

---
+++ class-note/1_type-basic.ts
@@ -0,0 +1,23 @@
+// TS 문자열
+let str: string = "Hello World";
+
+// TS 숫자
+let num: number = 10;
+
+// TS 배열
+let arr: Array<string> = ['a', 'b', 'c'];
+let arr2: Array<number> = [1, 2, 3];
+let items: number[] = [1,2,3]
+
+// TS 튜플
+let address: [string, number] = ['gangname', 100];
+
+// TS 객체
+let obj: object = {};
+let person: { name: string, age: number } = {
+    name: 'hm',
+    age: 20
+}
+
+// TS 진위값
+let show: boolean = false;
